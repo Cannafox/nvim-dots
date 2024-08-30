754f56d39729c17d7bab0ce9c59614702407e2f4
Cannafox <gklodkowski@gmail.com>
2024-08-30 03:32:28 +0200

    init commit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,31 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.opt.guicursor = ""
 
-local g = vim.g
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
-g.python3_host_prog = "/home/fox/.cache/pypoetry/virtualenvs/neovim-venv-_90-YO3t-py3.12/bin/python3"
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+return {
+    'nvim-telescope/telescope.nvim'
+    , branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+        vim.keymap.set('n', '<leader>fw', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        vim.keymap.set('n', '<leader>fg', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
+    end
+}
