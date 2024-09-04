4b703968f3b656b05a9bab1f32f63a197253b038
gklodkox <gracjanx.klodkowski@intel.com>
2024-09-04 21:49:55 +0200

    fix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -52,17 +52,17 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end
 })
 -- LSP signs
--- local sign = function(opts)
---   vim.fn.sign_define(opts.name, {
---     texthl = opts.name,
---     text = opts.text,
---     numhl = ''
---   })
--- end
--- sign({name = 'DiagnosticSignError', text = ''})
--- sign({name = 'DiagnosticSignWarn', text = ''})
--- sign({name = 'DiagnosticSignHint', text = ''})
--- sign({name = 'DiagnosticSignInfo', text = ''})
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+sign({name = 'DiagnosticSignError', text = ''})
+sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignHint', text = ''})
+sign({name = 'DiagnosticSignInfo', text = ''})
 
 
 return {
