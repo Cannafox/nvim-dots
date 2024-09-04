77e724d83497b81bb95a35be40d78565a018baf6
gklodkox <gracjanx.klodkowski@intel.com>
2024-09-04 15:20:52 +0200

    test

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,46 +1,27 @@
+local get_current_branch = function()
+  local cmdline = "git rev-parse --abbrev-ref HEAD"
+  local branch = vim.fn.system(cmdline):gsub("\n", "")
+  return branch
+end
+
 return {
     "tpope/vim-fugitive",
-    config = function()
-        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+    cmd = "Git",
+    keys = {
+      {"<leader>gss", "<cmd>Git status<cr>", desc = "Git status"},
+      {"<leader>gl", "<cmd>Git pull --rebase<cr>", desc = "Git pull (rebase)"},
+      {"<leader>gp", "<cmd>Git push<cr>", desc = "Git push"},
+      {"<leader>gP", "<cmd>Git push -u origin "..get_current_branch().."<cr>", desc = "Git push (set origin)"},
+      {"<leader>ga", "<cmd>Git add .<cr>", desc = "Git add all"},
+      {"<leader>gA", "<cmd>Git add %<cr>", desc = "Git add current_file"},
+      {"<leader>gsb", "<cmd>echo '"..get_current_branch().."'<cr>", desc = "Git show current branch"},
+      {"<leader>gcb", "<cmd>Telescope git_branches<cr>", desc = "Git checkout to existing branch"},
+      {"<leader>gcB", "<cmd>Git checkout -b ", desc = "Git checkout to existing branch"},
+      {"<leader>gcm", "<cmd>Git commit -m ", desc = "Git commit"},
+      {"<leader>gcM", "<cmd>Git commit --allow-empty -m 'Reset CI'<cr>", desc = "Git commit (empty)"},
 
-        local fugitive_group = vim.api.nvim_create_augroup("fugitive_group", {})
-
-        local autocmd = vim.api.nvim_create_autocmd
-        autocmd("BufWinEnter", {
-            group = fugitive_group,
-            pattern = "*",
-            callback = function()
-                if vim.bo.ft ~= "fugitive" then
-                    return
-                end
-
-                local bufnr = vim.api.nvim_get_current_buf()
-                -- git push
-                vim.keymap.set("n", "<leader>gp", function()
-                    vim.cmd.Git('push')
-                end, { buffer = bufnr, remap = false, desc = "Git push" })
-
-                -- git status
-                vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { buffer = bufnr, remap = false, desc = "Git push" })
-                -- git add all
-                vim.keymap.set("n", "<leader>ga", function()
-                    vim.cmd.Git({'add',  '--', '.'})
-                end, { buffer = bufnr, remap = false, desc = "Git push" })
-                -- git add current
-                vim.keymap.set("n", "<leader>gA", function()
-                    vim.cmd.Git({'add',  vim.fn.expand('%:p')})
-                end, { buffer = bufnr, remap = false, desc = "Git push" })
-                -- rebase always
-                vim.keymap.set("n", "<leader>gl", function()
-                    vim.cmd.Git({'pull',  '--rebase'})
-                end, { buffer = bufnr, remap = false, desc = "Git push" })
-
-                -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-                -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>gP", ":Git push -u origin ", { buffer = bufnr, remap = false, desc = "Git push" })
-            end,
-        })
-        vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
-        vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
-    end
+    }
+    --     vim.keymap.set("n", "<leader>gd", "<cmd>diffget //2<CR>", { buffer = bufnr, remap = false, desc = "Git diff 2" })
+    --     vim.keymap.set("n", "<leader>gD", "<cmd>diffget //3<CR>", { buffer = bufnr, remap = false, desc = "Git diff 3" })
+    -- end,
 }
