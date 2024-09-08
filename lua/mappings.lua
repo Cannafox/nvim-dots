2908115d79edfb70d54abd88497eb187e20409ae
Cannafox <gklodkowski@gmail.com>
2024-09-09 00:08:22 +0200

    asd

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,16 +1,13 @@
-vim.g.mapleader = ","
-vim.g.maplocalleader = "\\"
-
 -- Explorator
 vim.keymap.set("n", "\\", vim.cmd.NvimTreeToggle)
 
--- Dashboard 
+-- Dashboard
 vim.keymap.set("n", "|", ":Dashboard<CR>")
 
 -- Easy terminal escape
 vim.keymap.set('t', '<Esc>', "<C-\\><C-n>")
 
--- Move lines 
+-- Move lines
 vim.keymap.set('v', '<S-Down>', ":m '>+1<CR>gv=gv")
 vim.keymap.set('v', '<S-Up>', ":m '<-2<CR>gv=gv")
 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,42 @@
+local highlight = {
+  "RainbowRed", "RainbowYellow", "RainbowBlue",
+  "RainbowOrange", "RainbowGreen", "RainbowViolet",
+  "RainbowCyan"
+}
 return {
+    { "nvim-lua/plenary.nvim" },
     { "ryanoasis/vim-devicons" },
     { 'norcalli/nvim-colorizer.lua' },
     { "echasnovski/mini.icons", version = false },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = "User FilePost",
+        dependencies = { "HiPhish/rainbow-delimiters.nvim" },
+        opts = function()
+          return require("configs.indent_blankline")
+        end,
+        config = function(_, opts)
+            vim.g.rainbow_delimiters = { highlight = highlight }
+            local hooks = require "ibl.hooks"
+            hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+                vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+                vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+                vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+                vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+                vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+                vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+                vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+            end)
+            hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+            hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+            require("ibl").setup(opts)
+        end
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+        opts = function()
+          return require("configs.nvimtree")
+        end
+    }
 }
